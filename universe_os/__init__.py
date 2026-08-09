2cf2a2b8cf19861b261a6b6303450dd58e2e0fdf
osskosc-lab <osskosc@gmail.com>
2026-08-09 17:44:08 +0900

    Add Universe OS package

---
+++ universe_os/__init__.py
@@ -0,0 +1,27 @@
+"""TROS / Universe OS experimental kernel."""
+
+from .core import (
+    State,
+    WorldModelCore,
+    TimelineEngine,
+    CausalityEngine,
+    WillCore,
+    EchoCore,
+    EthicsLayer,
+    SelfReflectionCore,
+    TROSAgent,
+    MPCBaseline,
+)
+
+__all__ = [
+    "State",
+    "WorldModelCore",
+    "TimelineEngine",
+    "CausalityEngine",
+    "WillCore",
+    "EchoCore",
+    "EthicsLayer",
+    "SelfReflectionCore",
+    "TROSAgent",
+    "MPCBaseline",
+]
